08c0072ff25c9613924eb7242453ffe3daa8b2c0
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-12-16 02:14:48 -0500

    please work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-
+# Need to import csv from assets !
 require 'csv'  
 filename = File.read("/Users/shawnaejohnson/Downloads/Black_Owned_Restaurants.csv")
 
@@ -10,4 +10,20 @@ csv.each do |row|
   Restaurant.create!(row.to_hash)
 end
 
-puts "Done seeding!"
+puts "Done seeding! "
+
+
+#require 'csv'  
+#filename = File.read("assets/Black_Owned_Restaurants.csv")
+
+# csv = CSV.parse(filename, :headers => true)
+# csv.each do |row|
+#   new_hash = {}
+#   row.to_hash.each_pair do |k,v|
+#   new_hash.merge!({k.downcase => v})
+# end
+
+# Restaurant.create!(new_hash)
+# end
+
+# puts "Done seeding!"
