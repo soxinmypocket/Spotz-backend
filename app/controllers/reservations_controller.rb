7c2e2bdfb6de80732a0581a6e0d716cf6f357cf8
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-12-16 02:21:07 -0500

    reservation controller updated

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,36 @@
 class ReservationsController < ApplicationController
+    def show
+        @reservation = Reservation.find(params[:id])
+    end
+
+    def new
+        @reservation = Reservation.new
+    end
+
+    def create
+        @reservation = Reservation.create(reservation_params)
+        #redirect_to showreservations_path(params[:reservation][:user_id])
+    end
+
+    def edit      
+        @reservation = Reservation.find(params[:id])    
+    end
+
+    def update        
+        @reservation = Reservation.find(params[:id])        
+        @reservation.update(reservation_params)  
+        #redirect_to showreservations_path(params[:reservation][:user_id])    
+    end 
+
+    def destroy       
+       @reservation = Reservation.find(params[:id])        
+       @reservation.destroy    
+       #redirect_back fallback_location: showreservations_path #(@reservation)    
+    end
+
+    private
+
+    def reservation_params
+    params.require(:reservation).permit(:restaurant_name, :party_size, :date, :time, :user_id, :restaurant_id)
+    end
 end
